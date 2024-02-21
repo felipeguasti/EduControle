b62dbca32ea865db50ec58f74a6ee3dcb308a74a
Glitch (lavish-material-lord) <none>
2024-02-21 23:06:43 +0000

    🌦✊ Checkpoint ./app.js:64695080/564 ./src/config/db.js:64695080/922

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-
 // URL de conexão com o MongoDB
-const mongoURI = process.env.MONGODB_URI || 'mongodb://atlas-sql-65d4e1a0c6c9a87766f45303-zsuox.a.query.mongodb.net/equipreserve?ssl=true&authSource=admin';
+const mongoURI = process.env.MONGODB_URI;
 
 // Conexão com o MongoDB
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
