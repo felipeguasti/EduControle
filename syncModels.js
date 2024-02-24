c684744978019d08560ca8bd475b98138ffddeb2
Glitch (lavish-material-lord) <none>
2024-02-24 16:31:00 +0000

    🌞🚿 Checkpoint ./app.js copy:64695080/2212 ./syncModels.js:64695080/2961 ./src/public/js/index.js:64695080/10093 ./src/public/js/reserva.js:64695080/46930

---
+++ syncModels.js
@@ -0,0 +1,18 @@
+const sequelize = require('./src/config/db'); // Importe a instância do Sequelize com as configurações do seu banco de dados
+const Reserva = require('./src/models/reserva');
+const Recado = require('./src/models/recado');
+const Anuncio = require('./src/models/anuncio');
+const Usuario = require('./src/models/Usuario');
+
+async function sincronizarModelos() {
+  try {
+    // Sincroniza os modelos com o banco de dados
+    await sequelize.sync({ force: true }); // A opção { force: true } força a sincronização e recria as tabelas
+
+    console.log('Tabelas sincronizadas com sucesso.');
+  } catch (error) {
+    console.error('Erro ao sincronizar tabelas:', error);
+  }
+}
+
+sincronizarModelos();
