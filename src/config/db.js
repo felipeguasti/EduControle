54bfa3d5b1ff950af160b6afdec04bdd897aa97e
Glitch (careful-hospitable-princess) <felipe.guasti@hotmail.com>
2024-02-26 21:09:47 -0300

    Atualizei o banco de dados

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,34 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("u612973268_broadcast", "u612973268_broadcast", "E1=iTrLXsvk", {
-    host: "195.35.61.61",
-    dialect: "mysql",
-    logging: false,
+// Credenciais do banco de dados externo
+// const database = "u612973268_broadcast";
+// const username = "u612973268_broadcast";
+// const password = "E1=iTrLXsvk";
+// const host = "195.35.61.61"; // Endereço IP do banco de dados externo
+// const dialect = "mysql";
+
+// Credenciais do banco de dados local (para a área de testes)
+const database = "nome_do_banco_de_dados_local";
+const username = "nome_do_usuario_local";
+const password = "senha_do_usuario_local";
+const host = "localhost"; // O MySQL está rodando localmente, então o host é localhost
+const dialect = "mysql";
+
+// Inicializar a conexão com o banco de dados
+const sequelize = new Sequelize(database, username, password, {
+    host: host,
+    dialect: dialect,
+    logging: false, // Define se as mensagens de log do Sequelize serão exibidas (desabilitado neste caso)
 });
 
+// Testar a conexão
 sequelize.authenticate()
-    .then(() => console.log('Conexão com MySQL estabelecida com sucesso!'))
-    .catch(err => console.error('Erro ao conectar ao MySQL:', err));
+    .then(() => {
+        console.log('Conexão com MySQL estabelecida com sucesso! (Banco de dados local)');
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao MySQL: (Banco de dados local)', err);
+    });
 
+// Exportar a instância do Sequelize para ser utilizada em outras partes do projeto
 module.exports = sequelize;
